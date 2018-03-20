af206552dfba28917ab298d7dba78f80e78b4c47
icaka98 <minkov.h@gmail.com>
2018-03-20 20:14:47 +0100

    Updating Graphics

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javafx.animation.Interpolator;
 import javafx.animation.PathTransition;
 import javafx.application.Application;
 import javafx.geometry.Point2D;
@@ -10,25 +9,81 @@ import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main extends Application {
     private double startX, startY, finishX, finishY, tolerance;
 
     private Line aiming;
     private Pane mainPane;
+    private Circle ball;
 
     private void init(Course course) {
         this.startX = course.getStart().getX();
-        this.startY = course.getStart().getX();
+        this.startY = course.getStart().getY();
 
         this.finishX = course.getGoal().getX();
-        this.finishY = course.getGoal().getX();
+        this.finishY = course.getGoal().getY();
         this.tolerance = course.getToleranceRadius();
     }
 
     private double calculateFunction(double x, double y){
         return x * y + 20_000;
+    }
+
+    private List<ShortMove> getMoves(){
+        List<ShortMove> moves = new ArrayList<>();
+
+        double initialVelocity = 150;
+
+        moves.add(new ShortMove(100, 100, initialVelocity));
+        moves.add(new ShortMove(125, 125, initialVelocity));
+        moves.add(new ShortMove(150, 150, initialVelocity));
+        moves.add(new ShortMove(175, 175, initialVelocity));
+        moves.add(new ShortMove(200, 200, initialVelocity));
+        moves.add(new ShortMove(225, 225, initialVelocity));
+
+        Collections.reverse(moves);
+        System.out.println(String.join(", ",
+                moves.stream().map(p -> p.getFinalX() + "").collect(Collectors.toList())));
+
+        return moves;
+    }
+
+    private void createNextTransition(List<ShortMove> moves, int idx){
+        ShortMove move = moves.get(idx);
+
+        Line path = new Line(
+                this.ball.getCenterX(),
+                this.ball.getCenterY(),
+                move.getFinalX(),
+                move.getFinalY());
+
+        double endX = path.getEndX();
+        double endY = path.getEndY();
+
+        double len = Math.sqrt(Math.pow(path.getBoundsInLocal().getHeight(), 2)
+                + Math.pow(path.getBoundsInLocal().getWidth(), 2));
+
+        PathTransition transition = new PathTransition();
+        transition.setNode(this.ball);
+        transition.setDuration(Duration.seconds(len / move.getVelocity()));
+        transition.setPath(path);
+        transition.setCycleCount(1);
+        transition.setOnFinished(ob -> {
+            this.ball.setCenterX(endX);
+            this.ball.setCenterY(endY);
+
+            System.out.println(this.ball.getCenterX() + " --- " + this.ball.getCenterY());
+
+            if(idx + 1 < moves.size()){
+                createNextTransition(moves, idx + 1);
+            }
+        });
+        transition.play();
     }
 
     @Override
@@ -37,7 +92,7 @@ public class Main extends Application {
         this.mainPane = new Pane();
 
         Course exampleCourse = new Course(9.81, 0.5, 3,
-                new Point2D(250, 250), new Point2D(100, 150), 20,
+                new Point2D(250, 250), new Point2D(100, 100), 20,
                 null, null);
 
         init(exampleCourse);
@@ -47,7 +102,7 @@ public class Main extends Application {
         Circle hole = new Circle(this.finishX, this.finishY, this.tolerance, Color.BLACK);
         hole.setOpacity(.6);
 
-        Circle ball = new Circle(this.startX, this.startY, 10, Color.WHITE);
+        this.ball = new Circle(this.startX, this.startY, 10, Color.WHITE);
 
         for(double x = - Constants.SCENE_WIDTH / 2; x < Constants.SCENE_WIDTH / 2 ; x += 3.5){
             for(double y = - Constants.SCENE_HEIGHT / 2; y < Constants.SCENE_HEIGHT / 2; y += 3.5){
@@ -82,23 +137,15 @@ public class Main extends Application {
         });
 
         ball.setOnMouseReleased(event -> {
+            List<ShortMove> moves = this.getMoves();
 
-
-            PathTransition transition = new PathTransition();
-            transition.setNode(ball);
-            transition.setDuration(Duration.seconds(1.6));
-            transition.setPath(aiming);
-            transition.setCycleCount(1);
-            transition.setInterpolator(Interpolator.EASE_OUT);
-            transition.play();
-
-            ball.setCenterX(aiming.getEndX());
-            ball.setCenterY(aiming.getEndY());
+            this.createNextTransition(moves, 0);
 
             aiming.setEndY(0);
             aiming.setEndX(0);
             aiming.setStartY(0);
             aiming.setStartX(0);
+            aiming.setStrokeWidth(0.0);
         });
 
         this.mainPane.getChildren().add(ball);
