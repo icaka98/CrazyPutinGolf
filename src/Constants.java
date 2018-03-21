4dc62eb0aedf24c70ee568d9dfec0d109389af6c
icaka98 <minkov.h@gmail.com>
2018-03-21 03:02:11 +0100

    Updating the constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,8 +1,8 @@
 public class Constants {
     public static final String STAGE_TITLE = "Crazy Putin";
 
-    public static final int SCENE_WIDTH = 600;
+    public static final int SCENE_WIDTH = 500;
 
-    public static final int SCENE_HEIGHT = 600;
+    public static final int SCENE_HEIGHT = 500;
 
 }
