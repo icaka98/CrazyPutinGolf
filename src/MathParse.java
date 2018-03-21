6336929809ea530b514bd7933f061d453eabf598
Rhys Evans <34407907+dalekdoom@users.noreply.github.com>
2018-03-21 17:22:09 +0100

    MathParse

--- src/MathParse.java
+++ src/MathParse.java
@@ -6,13 +6,13 @@ import javax.script.ScriptException;
 public class MathParse{
     public static void main(String[] args) throws ScriptException {
         Map<String, String> values = new HashMap<String, String>();
-        values.put("y^2", "Math.pow(y, 2)"); // problem where I can't replace around the exponent to close the bracket
+        values.put("y^2", "Math.pow(2, 2)"); // problem where I can't replace around the exponent to close the bracket
         values.put("x^", "Math.pow(x, ");    // as denoted here
         values.put("sin", "Math.sin");
         values.put("cos", "Math.cos");
         values.put("z = ", "");
         values.put("pi", "Math.PI");
-        String expression = "z = sin(pi) + x^2";
+        String expression = "z = sin(pi) + y^2";
         System.out.println(expression);
 
         for (Map.Entry<String, String> entry : values.entrySet())
