9ce59c95467c7b612a47c206310b9fc16560b388
mcoenegracht <mathieucoenegracht@gmail.com>
2018-03-23 10:31:45 +0100

    added javadoc for node class

--- src/Utils/Node.java
+++ src/Utils/Node.java
@@ -1,10 +1,18 @@
 package Utils;
 
+/**
+ * @author Mathieu Coenegracht
+ * Node class that makes up the expression tree
+ */
 public class Node {
     public String value;
     public Node left;
     public Node right;
 
+    /**
+     * Constructor for the node class
+     * @param val String to be stored in the node
+     */
     public Node(String val){
         this.value = val;
         this.left = this.right = null;
