001dea53a95c4b36f8435c72d92557013dd663c0
icaka98 <minkov.h@gmail.com>
2018-03-23 04:12:38 +0100

    Fixing design of FileWriter

--- src/Utils/FileWriter.java
+++ src/Utils/FileWriter.java
@@ -4,25 +4,22 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class FileWriter {
-
-    public void writeToFile(double g, double mu, double vmax, double startX, double startY, double goalX, double goalY, double tolerance, String height){
-
-        String fileName = "DataFile";
+    public static void writeToFile(double g, double mu, double vmax,
+                            double startX, double startY, double goalX,
+                            double goalY, double tolerance, String function){
         try{
-            PrintWriter writer = new PrintWriter(fileName);
+            PrintWriter writer = new PrintWriter(Constants.DEFAULT_SAVE_FILE);
+
             writer.println("g = " + g);
             writer.println("mu = " + mu);
             writer.println("vmax = " + vmax);
             writer.println("start = (" + startX + "," + startY + ")");
             writer.println("goal = (" + goalX + "," + goalY + ")");
             writer.println("tolerance = " + tolerance);
-            writer.println("height = " + height);
+            writer.println("height = " + function);
             writer.close();
-            System.out.println("Write to file Done!");
         }catch(FileNotFoundException e){
             e.printStackTrace();
         }
-
-
     }
 }
