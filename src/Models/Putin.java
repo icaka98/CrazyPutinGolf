39f5d66b05d7cc1306afafef30d45b0f543bda55
Zhecho <zhecho15@yahoo.com>
2018-04-28 16:33:46 +0200

    slightly improved bot with higher mutation rate

--- src/Models/Putin.java
+++ src/Models/Putin.java
@@ -44,12 +44,14 @@ public class Putin {
         Collections.sort(this.population);
         for (Point p: this.population)
         {
-            System.out.println(p.getDistanceToGoal());
+            System.out.println("distance: " + p.getDistanceToGoal() + " X: " + p.getVelocityX() + " Y: " + p.getVelocityY());
         }
 
         while (this.population.get(0).getDistanceToGoal() > 0.1)
         {
             reproduce();
+            System.out.println("distance: " + this.population.get(0).getDistanceToGoal()); //+ " X: " + this.population.get(0).getVelocityX() + " Y: " + this.population.get(0).getVelocityY());
+            System.out.println("distance: " + this.population.get(1).getDistanceToGoal()); //+ " X: " + this.population.get(0).getVelocityX() + " Y: " + this.population.get(0).getVelocityY());
         }
 
         return population.get(0);
@@ -57,16 +59,31 @@ public class Putin {
 
     private void reproduce() {
         ArrayList<Point> newIndividuals = new ArrayList<>();
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 10; i++) {
             Point current = this.population.get(i);
-            for (int j = 0; j < 6; j++) {
+            for (int j = 0; j < 10; j++) {
                 Point other = this.population.get(j);
 
                 engine.setCurrentX(engine.getTerrainState().getStart().getX());
                 engine.setCurrentY(engine.getTerrainState().getStart().getY());
 
+
                 double velocityX = (current.getVelocityX() + other.getVelocityX())/2;
                 double velocityY = (current.getVelocityY() + other.getVelocityY())/2;
+
+                Random rnd  = new Random();
+
+                double change = rnd.nextDouble();
+                if(change< 0.9)
+                {
+                    velocityX += change- 0.45;
+                }
+
+                change = rnd.nextDouble();
+                if(change< 0.9)
+                {
+                    velocityY += change - 0.45;
+                }
 
                 engine.takeVelocityOfShot(velocityX, velocityY);
                 engine.executeShot();
