7105dda9d7d9b9af05fc5bbaaede3942e770f8ff
Zhecho <zhecho15@yahoo.com>
2018-05-27 00:05:48 +0200

    commiting a working code. It's not that difficult

--- src/Models/Function.java
+++ src/Models/Function.java
@@ -107,14 +107,14 @@ public class Function{
      * @return output value of the function dependent on the two input values
      */
     public double solve(double x, double y){ //solve equation for x and y values
-        /*vars.put("x", x);
+        vars.put("x", x);
         vars.put("y", y);
 
         //System.out.println(vars.containsKey("pi"));
         return eval(z);
-        */
+
         //TODO Tell silvia the domain and range of our course so she can implement this...!
-        Interpolator.designCourse(x,y);
+        //Interpolator.designCourse(x,y);
     }
 
     //public double Interpolation(double x, double y){
