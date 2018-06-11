2bd4e3e25512ebe123b225d43eb761bca7539266
Zhecho <zhecho15@yahoo.com>
2018-06-11 15:44:29 +0200

    removed unnecessary Spline classes

--- src/Models/Interpolation2D.java
+++
@@ -1,9 +0,0 @@
-package Models;
-
-public interface Interpolation2D {
-
-    /**
-     * Interpolate the data at a given 2-dimensional point.
-     */
-    double interpolate(double x1, double x2);
-}
