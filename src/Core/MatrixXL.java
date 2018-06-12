9a44b8731c4bae1882dc0906466930f346ae99ee
icaka98 <minkov.h@gmail.com>
2018-06-12 17:08:43 +0200

    Refactor MatrixXL class

--- src/Core/MatrixXL.java
+++ src/Core/MatrixXL.java
@@ -1,90 +1,92 @@
 package Core;
 
 public class MatrixXL {
-
-    private double[][] leftFactor;
-    private double[][] rightFactor;
-
+    private double[][] leftFactor, rightFactor;
     private double[][] setStuff;
-    private double[][] pointMatrix = new double[6][6];
+    private double[][] pointMatrix;
 
     public MatrixXL(){
-        setStuff = new double[4][4];
+        this.pointMatrix = new double[6][6];
+        this.setStuff = new double[4][4];
+
+        this.initFactors();
+        this.newPointMatrix();
+    }
+
+    private void initFactors() {
         this.leftFactor = new double[][]{
                 { 1, 0, 0, 0 },
                 { 0, 0, 1, 0 },
                 { -3, 3, -2, -1 },
                 { 2, -2, 1, 1 },
         };
+
         this.rightFactor = new double[][]{
                 { 1, 0, -3, 2 },
                 { 0, 0, 3, -2 },
                 { 0, 1, -2, 1 },
                 { 0, 0, -1, 1 },
         };
-
-        newPointMatrix();
     }
 
-    public void newPointMatrix() {
-
-        pointMatrix[0][0]=-1;
-        pointMatrix[0][1]=-1;
-        pointMatrix[0][2]=-1;
-        pointMatrix[0][3]=1;
-        pointMatrix[0][4]=1;
-        pointMatrix[0][5]=0;
-        pointMatrix[1][0]=-1;
-        pointMatrix[1][1]=-1;
-        pointMatrix[1][2]=2;
-        pointMatrix[1][3]=3;
-        pointMatrix[1][4]=1;
-        pointMatrix[1][5]=0;
-        pointMatrix[2][0]=-1;
-        pointMatrix[2][1]=1;
-        pointMatrix[2][2]=1;
-        pointMatrix[2][3]=2;
-        pointMatrix[2][4]=1;
-        pointMatrix[2][5]=2;
-        pointMatrix[3][0]=1;
-        pointMatrix[3][1]=2;
-        pointMatrix[3][2]=1;
-        pointMatrix[3][3]=0;
-        pointMatrix[3][4]=1;
-        pointMatrix[3][5]=2;
-        pointMatrix[4][0]=1;
-        pointMatrix[4][1]=2;
-        pointMatrix[4][2]=3;
-        pointMatrix[4][3]=4;
-        pointMatrix[4][4]=1;
-        pointMatrix[4][5]=4;
-        pointMatrix[5][0]=1;
-        pointMatrix[5][1]=1;
-        pointMatrix[5][2]=0;
-        pointMatrix[5][3]=2;
-        pointMatrix[5][4]=3;
-        pointMatrix[5][5]=4;
+    private void newPointMatrix() {
+        this.pointMatrix[0][0]=-1;
+        this.pointMatrix[0][1]=-1;
+        this.pointMatrix[0][2]=-1;
+        this.pointMatrix[0][3]=1;
+        this.pointMatrix[0][4]=1;
+        this.pointMatrix[0][5]=0;
+        this.pointMatrix[1][0]=-1;
+        this.pointMatrix[1][1]=-1;
+        this.pointMatrix[1][2]=2;
+        this.pointMatrix[1][3]=3;
+        this.pointMatrix[1][4]=1;
+        this.pointMatrix[1][5]=0;
+        this.pointMatrix[2][0]=-1;
+        this.pointMatrix[2][1]=1;
+        this.pointMatrix[2][2]=1;
+        this.pointMatrix[2][3]=2;
+        this.pointMatrix[2][4]=1;
+        this.pointMatrix[2][5]=2;
+        this.pointMatrix[3][0]=1;
+        this.pointMatrix[3][1]=2;
+        this.pointMatrix[3][2]=1;
+        this.pointMatrix[3][3]=0;
+        this.pointMatrix[3][4]=1;
+        this.pointMatrix[3][5]=2;
+        this.pointMatrix[4][0]=1;
+        this.pointMatrix[4][1]=2;
+        this.pointMatrix[4][2]=3;
+        this.pointMatrix[4][3]=4;
+        this.pointMatrix[4][4]=1;
+        this.pointMatrix[4][5]=4;
+        this.pointMatrix[5][0]=1;
+        this.pointMatrix[5][1]=1;
+        this.pointMatrix[5][2]=0;
+        this.pointMatrix[5][3]=2;
+        this.pointMatrix[5][4]=3;
+        this.pointMatrix[5][5]=4;
     }
 
     public double[][] getLeftFactor() {
-        return leftFactor;
+        return this.leftFactor;
     }
 
     public double[][] getRightFactor() {
-        return rightFactor;
+        return this.rightFactor;
     }
 
     public double[][] getPointMatrix() {
-        return pointMatrix;
+        return this.pointMatrix;
     }
 
     public void set(int x, int y, double z){
-        setStuff[x][y] = z;
+        this.setStuff[x][y] = z;
     }
 
     public double[][] getSetStuff(){
-        return setStuff;
+        return this.setStuff;
     }
 
-    public double getCoefficient (int i, int j) { return this.setStuff [i][j]; }
+    public double getCoefficient (int i, int j) { return this.setStuff[i][j]; }
 }
