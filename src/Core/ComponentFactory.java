770f38c945e07c10a8c6753bc73a204403538888
icaka98 <minkov.h@gmail.com>
2018-06-12 20:05:41 +0200

    Adding ComponentFactory

---
+++ src/Core/ComponentFactory.java
@@ -0,0 +1,43 @@
+package Core;
+
+import Utils.Constants;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+class ComponentFactory {
+    static Circle getHole(double finishX, double finishY, double tolerance){
+        Circle hole = new Circle(
+                finishX + Constants.FIELD_WIDTH / 2,
+                finishY + Constants.FIELD_HEIGHT / 2,
+                tolerance, Color.BLACK);
+
+        hole.setOpacity(0.6f);
+        return hole;
+    }
+
+    static Line getStopWall(){
+        Line stopLine = new Line(
+                Constants.DOWN_MID_LINE.getX1()+ Constants.FIELD_WIDTH / 2 + 8,
+                Constants.DOWN_MID_LINE.getY1() +Constants.FIELD_HEIGHT / 2 + 8,
+                Constants.DOWN_MID_LINE.getX2()+ Constants.FIELD_WIDTH / 2 - 8,
+                Constants.DOWN_MID_LINE.getY2() + Constants.FIELD_HEIGHT / 2 + 8);
+
+        stopLine.setStrokeWidth(Constants.WALL_THICKNESS);
+        return stopLine;
+    }
+
+    static Line getAiming(){
+        Line aiming = new Line(0, 0, 0, 0);
+        aiming.setStrokeWidth(0.0);
+
+        return aiming;
+    }
+
+    static Circle getBall(double startX, double startY){
+        return new Circle(
+                startX + Constants.FIELD_WIDTH / 2,
+                startY + Constants.FIELD_HEIGHT / 2,
+                Constants.BALL_RADIUS, Color.WHITE);
+    }
+}
