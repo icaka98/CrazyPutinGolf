a55a9cc568104ecb5388ce39c5ab8bb09755fca2
Feynlady <silviapixy98@gmail.com>
2018-06-13 13:20:16 +0200

    Matrix changes

--- src/Core/MatrixXL.java
+++ src/Core/MatrixXL.java
@@ -30,42 +30,42 @@ public class MatrixXL {
     }
 
     private void newPointMatrix() {
-        this.pointMatrix[0][0]=-1;
-        this.pointMatrix[0][1]=-1;
-        this.pointMatrix[0][2]=-1;
+        this.pointMatrix[0][0]=0;
+        this.pointMatrix[0][1]=1;
+        this.pointMatrix[0][2]=1;
         this.pointMatrix[0][3]=1;
         this.pointMatrix[0][4]=1;
-        this.pointMatrix[0][5]=0;
-        this.pointMatrix[1][0]=-1;
-        this.pointMatrix[1][1]=-1;
-        this.pointMatrix[1][2]=2;
-        this.pointMatrix[1][3]=3;
-        this.pointMatrix[1][4]=1;
+        this.pointMatrix[0][5]=1;
+        this.pointMatrix[1][0]=0;
+        this.pointMatrix[1][1]=0.05;
+        this.pointMatrix[1][2]=0.1;
+        this.pointMatrix[1][3]=0;
+        this.pointMatrix[1][4]=0;
         this.pointMatrix[1][5]=0;
-        this.pointMatrix[2][0]=-1;
+        this.pointMatrix[2][0]=0;
         this.pointMatrix[2][1]=1;
-        this.pointMatrix[2][2]=1;
-        this.pointMatrix[2][3]=2;
-        this.pointMatrix[2][4]=1;
-        this.pointMatrix[2][5]=2;
-        this.pointMatrix[3][0]=1;
-        this.pointMatrix[3][1]=2;
-        this.pointMatrix[3][2]=1;
-        this.pointMatrix[3][3]=0;
-        this.pointMatrix[3][4]=1;
-        this.pointMatrix[3][5]=2;
-        this.pointMatrix[4][0]=1;
-        this.pointMatrix[4][1]=2;
-        this.pointMatrix[4][2]=3;
-        this.pointMatrix[4][3]=4;
-        this.pointMatrix[4][4]=1;
-        this.pointMatrix[4][5]=4;
-        this.pointMatrix[5][0]=1;
-        this.pointMatrix[5][1]=1;
-        this.pointMatrix[5][2]=0;
-        this.pointMatrix[5][3]=2;
-        this.pointMatrix[5][4]=3;
-        this.pointMatrix[5][5]=4;
+        this.pointMatrix[2][2]=0.05;
+        this.pointMatrix[2][3]=0;
+        this.pointMatrix[2][4]=0.1;
+        this.pointMatrix[2][5]=0;
+        this.pointMatrix[3][0]=0;
+        this.pointMatrix[3][1]=0.05;
+        this.pointMatrix[3][2]=0.15;
+        this.pointMatrix[3][3]=0.2;
+        this.pointMatrix[3][4]=0.1;
+        this.pointMatrix[3][5]=0;
+        this.pointMatrix[4][0]=0;
+        this.pointMatrix[4][1]=1;
+        this.pointMatrix[4][2]=0;
+        this.pointMatrix[4][3]=0.05;
+        this.pointMatrix[4][4]=0.1;
+        this.pointMatrix[4][5]=0;
+        this.pointMatrix[5][0]=0;
+        this.pointMatrix[5][1]=0.05;
+        this.pointMatrix[5][2]=1;
+        this.pointMatrix[5][3]=1;
+        this.pointMatrix[5][4]=1;
+        this.pointMatrix[5][5]=1;
     }
 
     public double[][] getLeftFactor() {
