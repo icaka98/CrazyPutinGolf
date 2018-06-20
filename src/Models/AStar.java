a15a6b4a949a998c83c0643ffeb1b236ecb50d4d
mcoenegracht <mathieucoenegracht@gmail.com>
2018-06-20 15:48:22 +0200

    changed the search method so that it returns the path from start to goal

--- src/Models/AStar.java
+++ src/Models/AStar.java
@@ -1,12 +1,16 @@
 package Models;
+import java.util.Collections;
 import java.util.HashMap;
 import java.awt.Point;
 import java.util.PriorityQueue;
+
+import javafx.geometry.Point3D;
 import javafx.util.Pair;
+import java.util.ArrayList;
 
 public class AStar {
 
-    public static HashMap<Point, Point> Search(Grid graph, Point start, Point goal){
+    public static ArrayList<Point> search(Grid graph, Point start, Point goal){
         HashMap<Point, Point> cameFrom = new HashMap<Point,Point>();
         HashMap<Point, Double> costSoFar = new HashMap<Point, Double>();
 
@@ -20,8 +24,10 @@ public class AStar {
         while(!pq.isEmpty()){
             Point current = pq.poll().getKey();
 
-            if(current.equals(goal))
+            if(current.equals(goal)) {
+                System.out.println("Goal reached");
                 break;
+            }
 
             for(Point next : graph.neighbours(current)){
                 double newCost = costSoFar.get(current) + graph.cost(current, next);
@@ -29,16 +35,29 @@ public class AStar {
                 if (!costSoFar.containsKey(next) || newCost < costSoFar.get(next)) {
 
                     costSoFar.put(next,newCost);
-                    double priority = newCost + Heuristic(next, goal);
+                    double priority = newCost + heuristic(next, goal);
                     pq.add(new Pair(next, priority));
                     cameFrom.put(next, current);
                 }
             }
         }
-        return cameFrom;
+        return calculatePath(cameFrom, start, goal);
     }
 
-    private static double Heuristic(Point a, Point b){
+    private static ArrayList<Point> calculatePath(HashMap<Point,Point> cameFrom, Point start, Point goal){
+        ArrayList<Point> path = new ArrayList<>();
+        path.add(goal);
+
+        while(!goal.equals(start)){
+            //System.out.println("hi");
+            goal = cameFrom.get(goal);
+            path.add(goal);
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+    private static double heuristic(Point a, Point b){
         return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
     }
 }
