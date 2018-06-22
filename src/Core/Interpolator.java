2b6c699937b7ff9fb465d46114176d8f55f4cb47
Feynlady <silviapixy98@gmail.com>
2018-06-22 12:34:53 +0200

    Sorry
    Lol

--- src/Core/Interpolator.java
+++ src/Core/Interpolator.java
@@ -12,7 +12,7 @@ public class Interpolator {
         double[][] tmpResultMx = new double [4][4];
         double[][] finalResultMx = new double [4][4];
 
-        courseGrid = new MatrixXL[7][7];
+        courseGrid = new MatrixXL[5][5];
         double[][] pointMatrix = aCoefficients.getPointMatrix();
 
         aCoefficients.set(0,0, pointMatrix[a][b]);
@@ -52,7 +52,7 @@ public class Interpolator {
             mx.set(3, 0, (pointMx[i + 1][j+1] - pointMx[i][j+1]));
             mx.set(3, 1, (pointMx[i + 2][j+1] - pointMx[i][j+1]));
         }
-        else if (i==6) {
+        else if (i==4) {
             mx.set(2, 0, (pointMx[i + 1][j] - pointMx[i-1][j]));
             mx.set(2, 1, (pointMx[i + 1][j] - pointMx[i][j]));
             mx.set(3, 0, (pointMx[i + 1][j+1] - pointMx[i-1][j+1]));
@@ -73,7 +73,7 @@ public class Interpolator {
             mx.set(1, 2, (pointMx[i][j+2] - pointMx[i][j]));
             mx.set(1, 3, (pointMx[i+1][j+2] - pointMx[i+1][j]));
         }
-        else if (j==6) {
+        else if (j==4) {
             mx.set(0, 2, (pointMx[i][j+1] - pointMx[i][j-1]));
             mx.set(0, 3, (pointMx[i+1][j+1] - pointMx[i+1][j-1]));
             mx.set(1, 2, (pointMx[i][j+1] - pointMx[i][j]));
@@ -95,7 +95,7 @@ public class Interpolator {
                 mx.set(3, 2, ((pointMx[i+2][j+1] - pointMx[i+2][j])-(pointMx[i][j+1] - pointMx[i][j])));
                 mx.set(3, 3, ((pointMx[i+2][j+2] - pointMx[i+2][j])-(pointMx[i][j+2] - pointMx[i][j])));
             }
-            else if((j>0)&&(j<6)) {
+            else if((j>0)&&(j<4)) {
                 mx.set(2, 2, ((pointMx[i+1][j+1] - pointMx[i+1][j-1])-(pointMx[i][j+1] - pointMx[i][j-1])));
                 mx.set(2, 3, ((pointMx[i+1][j+2] - pointMx[i+1][j])-(pointMx[i][j+2] - pointMx[i][j])));
                 mx.set(3, 2, ((pointMx[i+2][j+1] - pointMx[i+2][j-1])-(pointMx[i][j+1] - pointMx[i][j-1])));
@@ -116,7 +116,7 @@ public class Interpolator {
                 mx.set(3, 2, ((pointMx[i+2][j+1] - pointMx[i+2][j])-(pointMx[i][j+1] - pointMx[i][j])));
                 mx.set(3, 3, ((pointMx[i+2][j+2] - pointMx[i+2][j])-(pointMx[i][j+2] - pointMx[i][j])));
             }
-            else if((j>0)&&(j<6)) {
+            else if((j>0)&&(j<4)) {
                 mx.set(2, 2, ((pointMx[i+1][j+1] - pointMx[i+1][j-1])-(pointMx[i-1][j+1] - pointMx[i-1][j-1])));
                 mx.set(2, 3, ((pointMx[i+1][j+2] - pointMx[i+1][j])-(pointMx[i-1][j+2] - pointMx[i-1][j])));
                 mx.set(3, 2, ((pointMx[i+2][j+1] - pointMx[i+2][j-1])-(pointMx[i][j+1] - pointMx[i][j-1])));
@@ -136,7 +136,7 @@ public class Interpolator {
                 mx.set(3, 2, ((pointMx[i+1][j+1] - pointMx[i+1][j])-(pointMx[i][j+1] - pointMx[i][j])));
                 mx.set(3, 3, ((pointMx[i+1][j+2] - pointMx[i+1][j])-(pointMx[i][j+2] - pointMx[i][j])));
             }
-            else if((j>0)&&(j<6)) {
+            else if((j>0)&&(j<4)) {
                 mx.set(2, 2, ((pointMx[i+1][j+1] - pointMx[i+1][j-1])-(pointMx[i-1][j+1] - pointMx[i-1][j-1])));
                 mx.set(2, 3, ((pointMx[i+1][j+2] - pointMx[i+1][j])-(pointMx[i-1][j+2] - pointMx[i-1][j])));
                 mx.set(3, 2, ((pointMx[i+1][j+1] - pointMx[i+1][j-1])-(pointMx[i][j+1] - pointMx[i][j-1])));
