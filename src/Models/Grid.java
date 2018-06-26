8c65b134bed12001597ef92e94d5761d62161f32
mcoenegracht <mathieucoenegracht@gmail.com>
2018-06-26 23:46:15 +0200

    changes to acommodate new obstacle representation

--- src/Models/Grid.java
+++ src/Models/Grid.java
@@ -3,15 +3,18 @@ import Utils.Constants;
 import Core.Controller;
 
 import java.awt.Point;
+import javafx.geometry.Point2D;
 import java.util.HashSet;
 import java.util.ArrayList;
+import javafx.scene.shape.Rectangle;
+import java.util.List;
 import java.awt.geom.Line2D;
 
 public class Grid {
 
     private int width, height;
     private HashSet<Point> obstacles = new HashSet<>();
-    private ArrayList<Line2D> walls;
+    private List<Rectangle> obs;
     private Controller control;
     //private Function f;
 
@@ -22,14 +25,24 @@ public class Grid {
     public Grid(Controller c){
         this.width = Constants.FIELD_WIDTH;
         this.height = Constants.FIELD_HEIGHT;
-        //this.walls = walls;
         this.control = c;
+        obs = c.getCourse().getObstacles();
 
-        for(int i = Constants.obstacle1[0]; i < Constants.obstacle1[1]; i++){
+        /*for(int i = Constants.obstacle1[0]; i < Constants.obstacle1[1]; i++){
 
             for(int j = Constants.obstacle1[2]; j < Constants.obstacle1[3]; j++) {
                 obstacles.add(new Point(i, j));
                 //System.out.println("x = "+i+" y = "+j);
+            }
+        }*/
+        System.out.println(obs.size());
+        for (Rectangle o : obs){
+            //System.out.println("HI");
+            for(int i = (int) o.getX(); i < (int)o.getX() + o.getWidth(); i++){
+                for(int j = (int)o.getY(); j < (int)o.getY() +o.getHeight(); j++) {
+                    obstacles.add(new Point(i, j));
+                    //System.out.println("x = "+i+" y = "+j);
+                }
             }
         }
 
@@ -52,7 +65,11 @@ public class Grid {
         for(int i = id.x-10; i < id.x+10; i++){
             for(int j = id.y-10; j < id.y+10;j++){
                 if(obstacles.contains(new Point(i,j))){return false;}
-
+                /*for(Rectangle o : obs){
+                    if(o.contains(new Point2D(id.x, id.y))){
+                        return false;
+                    }
+                }*/
 
                 if(!inBounds(new Point(i,j))){
                     return false;
