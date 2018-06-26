8f3828b033071c23cc6d513edd6bb1759bf290f0
icaka98 <minkov.h@gmail.com>
2018-06-26 21:24:48 +0200

    Update main menu

--- src/Core/MainStart.java
+++ src/Core/MainStart.java
@@ -5,19 +5,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainStart extends Application {
-    private Controller controller;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.controller = new Controller();
-        Scene scene = new Scene(new MainMenu(this.controller), 400, 500);
-
-
+        Scene scene = new Scene(new MainMenu(new Controller()), 600, 600);
 
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
